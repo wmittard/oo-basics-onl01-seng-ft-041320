640c54d10dc722c9225713c4b6b96d0c210575c0
William Mittard <billymittard@ymail.com>
2020-04-20 17:37:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   def initialize(brand)
     @brand = brand 
   end 
-  def cobble(new_condition)
+  def cobble
     puts "Your shoe is as good as new!"
   end
   def condition=(condition)
